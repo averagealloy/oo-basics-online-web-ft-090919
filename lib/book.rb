d6869ae547e1d0f3ee158b77e2f0655c39632dbc
Mike McTighe <m.mctighe10@gmail.com>
2019-09-17 17:37:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -27,7 +27,7 @@
 #    puts "Flipping the page...wow, you read fast!"
 #  end
 #end
-Book.new("And Then There Were None")
+#Book.new("And Then There Were None")
 
 class Book
   attr_accessor :author, :page_count, :genre
