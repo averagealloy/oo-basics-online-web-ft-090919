3a99e96bdf2a9a9594a8af492effdc5650caff13
Mike McTighe <m.mctighe10@gmail.com>
2019-09-17 17:50:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,8 @@
 # Make your shoe class here!
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
-  attr_reader :name 
-  def initialize(name)
+  attr_reader :name
+  def initialize(br)
     @name = name
   end
 
