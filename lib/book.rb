20f31d8a65c23da906117f137257d6d02c88ecc8
Mike McTighe <m.mctighe10@gmail.com>
2019-09-17 17:35:49 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -31,13 +31,11 @@ Book.new("And Then There Were None")
 
 class Book
   attr_accessor :author, :page_count, :genre
-  attr_reader :title 
+  attr_reader :title
   def initialize(title)
     @title = title
   end
   def title
-    @title
-  end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
