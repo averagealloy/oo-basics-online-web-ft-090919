8e4b9f72ffbc449ae4961580b66e2d10e67fc1ba
Mike McTighe <m.mctighe10@gmail.com>
2019-09-17 17:57:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :brand, :color, :size, :material, :condition, 
   attr_reader :name
   def initialize(brand)
     @brand = brand
@@ -8,6 +8,6 @@ class Shoe
 def cobble
   if shoe.condition == "old"
     shoe.condition = "new"
-  end 
+  end
 end
 Shoe.new("Nike")
