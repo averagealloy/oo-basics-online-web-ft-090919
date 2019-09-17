f0ea16cd793b0a893a3bcde42eb7e1c8bd73f6ef
Mike McTighe <m.mctighe10@gmail.com>
2019-09-17 17:36:42 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,8 +11,8 @@ class Book
   def author
     @author
   end
-  def page_count=(num)
-    @page_count = num
+#  def page_count=(num)
+#    @page_count = num
 #  end
 #  def page_count
 #    @page_count
